bf97b743b2451010ac8b6f3458fa057bdefcce10
enguyen93 <enguyenusaf93@gmail.com>
2020-01-27 14:18:49 -0600

    initial commit

---
+++ src/app/places/discover/discover.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../places.service';
+
+@Component({
+  selector: 'app-discover',
+  templateUrl: './discover.page.html',
+  styleUrls: ['./discover.page.scss'],
+})
+export class DiscoverPage implements OnInit {
+
+  private loadedPlaces = [];
+  constructor(private placesService: PlacesService) { }
+
+  ngOnInit() {
+    this.loadedPlaces = this.placesService.places;
+  }
+
+}
---
+++ src/app/places/places-routing.module.ts
@@ -0,0 +1,66 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { PlacesPage } from "./places.page";
+import { DiscoverPage } from "./discover/discover.page";
+import { OffersPage } from "./offers/offers.page";
+
+const routes: Routes = [
+  {
+    // This route will be starting with /places/tabs... because This routing module is inside Places
+    path: "tabs",
+    component: PlacesPage,
+    children: [
+      {
+        path: "discover",
+        children: [
+            {
+                path: '',
+                loadChildren: './discover/discover.module#DiscoverPageModule'
+            },
+            {
+                path: ':placeId',
+                loadChildren: './discover/place-detail/place-detail.module#PlaceDetailPageModule'
+            }
+        ]
+      },
+      {
+        path: "offers",
+        children: [
+            {
+                path: '',
+                loadChildren: './offers/offers.module#OffersPageModule'
+            },
+            {
+                path: 'new',
+                loadChildren: './offers/new-offer/new-offer.module#NewOfferPageModule'
+            },
+            {
+                path: ':placeId',
+                loadChildren: './offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule'
+            },
+            {
+                path: 'edit/"placeId',
+                loadChildren: './offers/edit-offer/edit-offer.module#EditOfferPageModule'
+            }
+        ]
+      },
+      {
+          path: '',
+          redirectTo: '/places/tabs/discover',
+          pathMatch: 'full'
+      }
+    ]
+  },
+  {
+      path: '',
+      redirectTo: '/places/tabs/discover',
+      pathMatch: 'full'
+  }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PlacesRoutingModule {}
